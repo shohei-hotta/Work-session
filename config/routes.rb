c6f56966a41ba6005d2d84692094636c7be6128a
shohei-hotta <sho1127-cal@kra.biglobe.ne.jp>
2020-04-22 05:05:57 +0000

    rootを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: "blogs#index"
+  root to: "sessions#new"
   resources :blogs
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
