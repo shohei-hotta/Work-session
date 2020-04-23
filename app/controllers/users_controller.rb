5439362f68d76514223ebdec09f6537dc7b9cfae
shohei-hotta <sho1127-cal@kra.biglobe.ne.jp>
2020-04-23 12:36:37 +0000

    users_controller.rbを一部変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @favorites = current_user.favorite_blogs
+    @favorites = @user.favorite_blogs
   end
 
   private
